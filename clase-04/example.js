657c7fdf3ab277fbe5d7ab772563ee6a8980afbd
mdcarron <mdcarron.dev@gmail.com>
2021-03-29 20:28:18 -0300

    Completa ejercios de clase y desafio

--- clase-04/example.js
+++ clase-04/example.js
@@ -1,3 +1,7 @@
+import { Observable } from "rxjs";
+
+// PULL SINGLE - FUNCTION
+/*
 function* idGenerator() {
   let i = 0;
   while (true) yield i++;
@@ -8,7 +12,10 @@ const gen = idGenerator();
 console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
+*/
 
+// PULL MULTI - ITERADOR
+/*
 function crearIterador(arr) {
   let nextId = 0;
 
@@ -27,3 +34,56 @@ console.log(iterador.next().value);
 console.log(iterador.next().value);
 // console.log(iterador.next().value);
 console.log(iterador.next().done);
+*/
+
+// PUSH SINGLE - PROMISE
+/*
+new Promise((res, rej) => {
+  setTimeout(() => {
+    res("Este ");
+  }, 1000);
+})
+  .then((res) => {
+    console.log(res);
+    return res + "es el ";
+  })
+  .then((res) => {
+    console.log(res);
+    return res + "texto ";
+  })
+  .then((res) => {
+    console.log(res);
+    return res + "completo.";
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log("ERROR: " + error);
+  });
+*/
+
+//PUSH SINGLE - OBSERVABLE
+const observable = new Observable((suscriber) => {
+  suscriber.next(1);
+  suscriber.next(2);
+  suscriber.next(3);
+  setTimeout(() => {
+    suscriber.next(4);
+    suscriber.complete();
+  }, 1000);
+});
+
+console.log("Antes de suscribirse al observable");
+observable.subscribe({
+  next(x) {
+    console.log("step ", x);
+  },
+  error(err) {
+    console.log("ERROR: ", err);
+  },
+  complete() {
+    console.log("Finish.");
+  },
+});
+console.log("Despues de la suscripcion al observable");
