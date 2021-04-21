53225bf7631f4e0a832673ebd81793ba67b9c9a6
mdcarron <mauri.carron@gmail.com>
2021-04-21 11:47:32 -0300

    Completa ejercicios de clase

--- clase-10/pag34/server.js
+++ clase-10/pag34/server.js
@@ -1,8 +1,34 @@
+import path from "path";
 import express from "express";
 import handlebars from "express-handlebars";
 
 const app = express();
 const PORT = 8080;
+let __dirname = path.resolve(path.dirname(""));
+
+const datos = {
+  nombre: "Sopita",
+  apellido: "Quick",
+  edad: "31",
+  email: "sopita.quick@gmail.com",
+  telefono: "2994987654",
+};
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("main", datos);
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server iniciado en el PUERTO: ${PORT}`);
